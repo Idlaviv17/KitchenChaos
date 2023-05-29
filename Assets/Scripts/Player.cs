05dac6b074474cd70f15f7a6c8eb6444346d96dd
Jose Angulo <jjavier1708@gmail.com>
2023-05-28 18:09:17 -0700

    Add the new input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,26 +6,12 @@ public class Player : MonoBehaviour {
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 12f;
+    [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
 
     private void Update() {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y++;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x--;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y--;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x++;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
